965385e172933478de93a58203ca25bacbb3a13c
Francis Louie G. Antonio <francis.antonio0927@gmail.com>
2023-01-15 21:09:21 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 
 function App() {
-
+  
   return (
     <div className="App">
       <Navbar />
@@ -17,14 +17,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
